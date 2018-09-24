75fc6f387b171b95f97f24c0b764ce3571b1ecde
gvallejos92 <gian.vallejos92@gmail.com>
2018-09-24 17:19:50 -0500

    Modals, Forms

--- resources/assets/js/container-general/index.vue
+++ resources/assets/js/container-general/index.vue
@@ -2,16 +2,18 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<b-alert show>Default Alert</b-alert>
+				<h3>Alerts: </h3>
+				<b-alert show variant="success">Success Alert</b-alert>
+				<b-alert show variant="danger">Error Alert</b-alert>
 			</div>
 			<div class="col-md-6">
 				<h3>Botones: </h3>		
 				<div class="botonesLayout pt-3">
-					<ButtonComponent url="#" name="General" tipo="info"/>
-					<ButtonComponent url="#" name="Guardar" tipo="guardar"/>
-					<ButtonComponent url="#" name="Nuevo" tipo="nuevo"/>
-					<ButtonComponent url="#" name="Modificar" tipo="modificar"/>
-					<ButtonComponent url="#" name="Cancelar" tipo="cancelar"/>
+					<b-button href="#" variant="primary">General</b-button>
+					<b-button href="#" variant="success">Guardar</b-button>
+					<b-button href="#" variant="secondary">Nuevo</b-button>
+					<b-button href="#" variant="warning">Modificar</b-button>
+					<b-button href="#" variant="danger">Cancelar</b-button>
 				</div>
 				
 				<div class="panelLayout pt-4">
@@ -22,7 +24,7 @@
 						<div class="table mt-3 mb-4" slot="body">
 						    <TableComponent :fields="ultimosPacientes" />
 						</div>
-						<ButtonComponent slot="footer" url="#" name="Ver Pacientes" tipo="info"/>
+						<b-button slot="footer" href="#" variant="primary">Ver Pacientes</b-button>
 					</PanelCard>
 				</div>
 
@@ -30,6 +32,8 @@
 					<h3>Charts</h3>
 					<PanelCard>
 						<GChart type="LineChart" :data="chartData" :options="chartOptions" slot="body" />
+
+						<GChart type="PieChart" :data="donutData" :options="donutOptions" :width="600" slot="body" />
 					</PanelCard>
 				</div>
 			</div>
@@ -52,13 +56,53 @@
 						<TableComponent :fields="items" slot="body" />
 					</PanelCard>
 				</div>
+
+				<div class="panelLayout pt-4">
+					<h3>Forms: </h3>					
+					<PanelCard>	
+						<b-form @submit="onSubmit" slot="body">
+							<b-form-group label="Nombre de Campo:">
+				        		<b-form-input
+				                      type="text"
+				                      v-model="form.name"
+				                      required
+				                      placeholder="Nombre de Campo">
+				        		</b-form-input>
+				      		</b-form-group>
+				      		<b-input-group>
+						      	<b-form-input type="text" v-model="form.other" placeholder="Ningun Elemento Seleccionado" disabled></b-form-input>
+						      	<b-input-group-append>
+							    	<b-btn class="pl-3 pr-3" variant="success">
+							    		<i class="fas fa-search"></i>
+							    	</b-btn>
+							    </b-input-group-append>
+						    </b-input-group>
+						    <div class="mt-3">
+				      			<b-button type="submit" variant="success">Guardar</b-button>
+				      			<b-button variant="danger">Cancel</b-button>
+				      		</div>
+				      	</b-form>
+			      	</PanelCard>			      
+				</div>
+
+				<div class="panelLayout pt-3">
+					<h3>Modal: </h3>
+					<div class="pt-1">
+						<b-btn v-b-modal.exampleModal variant="primary">Abrir Modal</b-btn>
+
+						<b-modal id="exampleModal" size="lg" title="Lista de Pacientes">
+	    					<h5>Test Modal</h5>
+	    					<b-button slot="modal-footer" variant="primary" size="sm">Ir a Pacientes</b-button>
+	  					</b-modal>
+  					</div>
+				</div>
 			</div>
 		</div>		
 	</div>
+
 </template>
 
 <script>
-	import ButtonComponent from '../button/button-component.vue'
 	import Dashbox from '../dashbox/dashbox-component.vue'
 	import PanelCard from '../panel/panel-component.vue'
 	import FormBuscar from '../form/form-buscar-component.vue'
@@ -68,7 +112,6 @@
 	export default{
 		name: 'Container-General',
 		components:{
-			ButtonComponent,
 			Dashbox,
 			PanelCard,
 			FormBuscar,
@@ -101,8 +144,32 @@
 			          title: 'Company Performance',
 			          subtitle: 'Sales, Expenses, and Profit: 2014-2017',
 			        }
-			    }
+			    },
+			    donutData: [
+		          ['Task', 'Hours per Day'],
+		          ['Work',     11],
+		          ['Eat',      2],
+		          ['Commute',  2],
+		          ['Watch TV', 2],
+		          ['Sleep',    7]
+		        ],
+		        donutOptions: {
+		        	chart: {
+		        		title: 'My Daily Activities',
+          				pieHole: 0.4,
+		        	}
+		        },
+		        form: {
+			        name: '',
+			        other: ''			        
+			    },
 			}
+		},
+		methods:{
+			onSubmit (evt) {
+		      evt.preventDefault();
+		      alert(JSON.stringify(this.form));
+		    }
 		}
 	}
 </script>
