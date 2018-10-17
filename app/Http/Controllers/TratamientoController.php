e00b5ac318e6458a5cdbc1ddf8e62c52db63764a
Vick <zhaynoth@gmail.com>
2018-10-17 12:00:47 -0500

    prices insertion as transaction

--- app/Http/Controllers/TratamientoController.php
+++ app/Http/Controllers/TratamientoController.php
@@ -45,15 +45,22 @@ class TratamientoController extends Controller{
 
     	if ($validator->passes()) {
             try{
+                DB::beginTransaction();
+
                 $tratamiento = new tratamiento();
                 $tratamiento->detalle = $request->detalle;
                 $tratamiento->save();
-                self::insertStandardPricesToAllCompanies($tratamiento->id, $request->precio_estandar);
+                $pricesInserted = self::insertStandardPricesToAllCompanies($tratamiento->id, $request->precio_estandar);
                 
-                $request->session()->flash('alert', json_encode(['type' => 'success', 'msg' => 'Tratamiento registrado correctamente']));
-                    
-                return response()->json(['success' => 'success']);
-
+                if($pricesInserted){
+                    DB::commit();
+                    $request->session()->flash('alert', json_encode(['type' => 'success', 'msg' => 'Tratamiento registrado correctamente']));
+                    return response()->json(['success' => 'success']);
+                }
+                else{
+                    DB::rollback();
+                    return response()->json(['error' => 'Ha ocurrido un error al insertar el tratamiento o sus precios']);
+                }
             }catch(Exception $e){
                 return response()->json(['error'=>$e->getMessage()]);
             }
@@ -63,9 +70,12 @@ class TratamientoController extends Controller{
 
     public function insertStandardPricesToAllCompanies($treatmentId, $price){
         $companies = DB::select('call OP_ObtenerEmpresas()');
+         
         foreach ($companies as $company) {
-            DB::select('call OP_AgregarPreciosPorEmpresa('.$company->id.','.$treatmentId.','.$price.')');
+            $status = DB::select('call OP_AgregarPreciosPorEmpresa('.$company->id.','.$treatmentId.','.$price.')');
+            if($status == 0) return false;
         }
+        return true;
     }
 
     public function update(Request $request, $id){
