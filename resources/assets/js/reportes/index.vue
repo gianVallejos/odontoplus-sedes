72812c57fe7f126e96d634d630fab689572875dd
Vick <zhaynoth@gmail.com>
2018-11-23 11:24:20 -0500

    fixes

--- resources/assets/js/reportes/index.vue
+++ resources/assets/js/reportes/index.vue
@@ -149,7 +149,7 @@
 				years: [
 					{ value: "2017", text: "2017" },
 					{ value: "2018", text: "2018" },
-					{ value: "2019", text: "2019" }
+					{ value: "2019", text: "2019" },
 					{ value: "2020", text: "2020" }
 				],
 				ingresosVSegresosChart: {
