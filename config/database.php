296c416f5bdb6ff40abc2ca6c553d7018da2fc09
Gian Piere <gian.vallejos92@gmail.com>
2018-11-29 18:46:42 -0500

    Correcciones 2

--- config/database.php
+++ config/database.php
@@ -63,21 +63,6 @@ return [
             'engine' => null,
         ],
 
-        '2_20601744750_CAJ' => [
-            'driver' => 'mysql',
-            'host' => '127.0.0.1',
-            'port' => '3306',
-            'database' => 'op_20601744750_bd',
-            'username' => 'root',
-            'password' => '',
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-        ],
-
 
     ],
 
