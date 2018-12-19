6dd5118d919d5779ca8ce167bb2f778925424fc1
Gian Piere <gian.vallejos92@gmail.com>
2018-12-19 09:52:20 -0500

    Footer update

--- resources/assets/js/footer/footerComponent.vue
+++ resources/assets/js/footer/footerComponent.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="footer">
 		<div class="container text-right">
-			Desarrollado por <a href="http://www.odontoplus.pe" target="_blank">Odontoplus</a> © 2018 v.3.1.1
+			Desarrollado por <a href="http://www.odontoplus.pe" target="_blank">Odontoplus</a> © 2018 v.3.1.3
 		</div>
 	</footer>
 </template>
