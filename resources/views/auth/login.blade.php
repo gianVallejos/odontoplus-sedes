8a5c1b055b39c206fdfdd4a007a5f346ff6e7c9a
Gian Piere <gian.vallejos92@gmail.com>
2019-01-31 10:04:00 -0500

    Update ganancias - citas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,7 +73,7 @@
 
     </div>
     <div class='sign text-center'>
-        <p>Software desarrollado por <a href="https://www.odontoplus.pe" target="_blank">Odontoplus</a> &copy; 2018 v. 3.1.10</p>
+        <p>Software desarrollado por <a href="https://www.odontoplus.pe" target="_blank">Odontoplus</a> &copy; 2018 v. 3.1.11</p>
     </div>
 
 </div>
