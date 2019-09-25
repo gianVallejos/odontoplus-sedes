053d9f6d990b382c3df68d9401779818f913e2e0
gvallejos92 <gian.vallejos92@gmail.com>
2019-09-24 21:58:26 -0500

    Version modification

--- resources/assets/js/footer/footerComponent.vue
+++ resources/assets/js/footer/footerComponent.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="footer">
 		<div class="container text-right">
-			Desarrollado por <a href="http://www.odontoplus.pe" target="_blank">Odontoplus</a> © 2018 v.3.1.13
+			Desarrollado por <a href="http://www.odontoplus.pe" target="_blank">Odontoplus</a> © 2018 v.3.1.14
 		</div>
 	</footer>
 </template>
