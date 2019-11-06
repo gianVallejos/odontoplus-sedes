520a60639858ec81ad4f62e2aa9a5aed39d02d77
gvallejos92 <gian.vallejos92@gmail.com>
2019-11-05 21:04:51 -0500

    Update responsive corrections

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,7 +18,7 @@
 	<link rel="stylesheet" href="{{ asset('css/all.min.css?v=1.0.2') }}">
 	<link rel="stylesheet" href="{{ asset('css/fonts.css?v=1.0.2') }}">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-	<link rel="stylesheet" href="{{ asset('css/odontoplus.css?v=1.0.43') }}">
+	<link rel="stylesheet" href="{{ asset('css/odontoplus.css?v=1.0.44') }}">
 	<link rel="stylesheet" href="{{ asset('css/header.css?v=1.0.5') }}">
 	<link rel="stylesheet" href="{{ asset('css/dashboard.css?v=1.0.1') }}">
 	<link rel="stylesheet" href="{{ asset('css/auth.css') }}" />
