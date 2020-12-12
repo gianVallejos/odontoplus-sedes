494c8a3866ca0e0b0d88ced50383e41e88c9ba95
Gian Vallejos <gian.vallejos92@gmail.com>
2020-12-12 01:47:11 -0500

    Test_update_4

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -28,7 +28,7 @@ class IngresoController extends Controller
     }
 
     public function index(){
-        $ingresos =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_all()');
+        $ingresos =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_all()');
         $ingresos = json_encode($ingresos);
 
         return view($this->path . '.index', compact('ingresos'));
@@ -36,9 +36,9 @@ class IngresoController extends Controller
 
     public function create()
     {
-        $pacientes =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Pacientes_get_all()');
+        $pacientes =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Pacientes_get_all()');
         $pacientes = json_encode($pacientes);
-        $doctores =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Doctors_get_all()');
+        $doctores =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Doctors_get_all()');
         $doctores = json_encode($doctores);
 
         return view($this->path . '.create', compact('pacientes', 'doctores'));
@@ -51,7 +51,7 @@ class IngresoController extends Controller
 
         if ($validator->passes()) {
             try{
-                $ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_add_all("'. $request->idPaciente .'")');
+                $ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_add_all("'. $request->idPaciente .'")');
                 if( $ingreso[0]->ESTADO > 0 ){
                   return response()->json(['success' => 'created']);
                 }else{
@@ -65,9 +65,9 @@ class IngresoController extends Controller
     }
 
     public function reporte($id){
-        $igeneral =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_all_Id('. $id .')')[0];
-        $idetalle =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_get_all_Id('. $id .')');
-        $paciente_sede =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Sedes_get_all_id("'. $igeneral->pacienteSedeId .'")')[0];
+        $igeneral =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_all_Id('. $id .')')[0];
+        $idetalle =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_get_all_Id('. $id .')');
+        $paciente_sede =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Sedes_get_all_id("'. $igeneral->pacienteSedeId .'")')[0];
         $igeneral = json_encode($igeneral);
         $idetalle = json_encode($idetalle);
         $paciente_sede = json_encode($paciente_sede);
@@ -77,18 +77,18 @@ class IngresoController extends Controller
 
     public function lineItem($id){
         try{
-            $ingresos =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_all_Id('. $id .')')[0];
+            $ingresos =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_all_Id('. $id .')')[0];
             $hc = $ingresos->hc;
             $ingresos = json_encode($ingresos);
-            $ingreso_detalle =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_get_all_Id('. $id .')');
+            $ingreso_detalle =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_get_all_Id('. $id .')');
             $ingreso_detalle = json_encode($ingreso_detalle);
-            $tratamientos =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Tratamientos_get_all_pacienteId('. $hc .')');
+            $tratamientos =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Tratamientos_get_all_pacienteId('. $hc .')');
             $tratamientos = json_encode($tratamientos);
-            $doctores =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Doctors_get_all()');
+            $doctores =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Doctors_get_all()');
             $doctores = json_encode($doctores);
-            $sedes = DB::connection(CurBD::getCurrentSchema())->statement('call OP_Sedes_get_all()');
+            $sedes = DB::connection(CurBD::getCurrentSchema())->select('exec OP_Sedes_get_all()');
             $sedes = json_encode($sedes);
-            $presupuestos_by_ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_presupuestos_Id("'. $id .'")');
+            $presupuestos_by_ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_presupuestos_Id("'. $id .'")');
             $presupuestos_by_ingreso = json_encode($presupuestos_by_ingreso);
 
             return view('ingresos.line-item', compact('ingresos', 'ingreso_detalle', 'tratamientos', 'doctores', 'sedes', 'presupuestos_by_ingreso'));
@@ -100,17 +100,17 @@ class IngresoController extends Controller
     public function lineItemSave(Request $request){
             try{
                 foreach( $request->trats as $trat ){
-                    $ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_add_all('. $request->ingresoId .', '. $trat['precioId'] .', '.
+                    $ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_add_all('. $request->ingresoId .', '. $trat['precioId'] .', '.
                                                                             $trat['cantidad'] .', '. $trat['monto'] .', '.$request->igv . ', ' . $trat['costo_variable'] . ', "' .
                                                                             $request->fecha .'", '. $request->doctor . ', "'.
                                                                             $request->codigo .'", '. $request->tipo_pago .', '. $request->sede .')');
                 }
-                $last_ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_get_ultimo_Id('. $request->ingresoId .')')[0];
+                $last_ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_get_ultimo_Id('. $request->ingresoId .')')[0];
 
-                $total_ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_monto_total_Id('. $request->ingresoId .')')[0];
+                $total_ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_monto_total_Id('. $request->ingresoId .')')[0];
                 $Ingresototal = $total_ingreso->total;
 
-                $ing_total =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_get_all_total_Id('. $last_ingreso->lastIngresoDetalle .')')[0];
+                $ing_total =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_get_all_total_Id('. $last_ingreso->lastIngresoDetalle .')')[0];
                 $mg = $ing_total->mg;
                 $mg_core = $ing_total->mg_core;
 
@@ -129,16 +129,16 @@ class IngresoController extends Controller
         //if ($validator->passes()) {
             try{
                 foreach( $request->trats as $trat ){
-                    $ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_update_all('. $request->ingresoId .', '. $trat['precioId'] .', '.
+                    $ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_update_all('. $request->ingresoId .', '. $trat['precioId'] .', '.
                                                                                $trat['cantidad'] .', '. $trat['monto'] .', '.$request->igv . ',"' .
                                                                                $request->fecha .'", '. $request->doctor . ', "'.
                                                                             $request->codigo .'", '. $request->tipo_pago .', '. $request->sede .', '. $id .')');
                 }
 
-                $total_ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_monto_total_Id('. $request->ingresoId .')')[0];
+                $total_ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_monto_total_Id('. $request->ingresoId .')')[0];
                 $Ingresototal = $total_ingreso->total;
 
-                $ing_total =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_get_all_total_Id('. $id .')')[0];
+                $ing_total =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_get_all_total_Id('. $id .')')[0];
                 $mg = $ing_total->mg;
                 $mg_core = $ing_total->mg_core;
 
@@ -153,7 +153,8 @@ class IngresoController extends Controller
 
     public function lineItemDelete($id){
         try{
-            $ingreso = DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_Detalle_delete_all('. $id .')');            
+            $ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_Detalle_delete_all('. $id .')');
+
             return response()->json(['success' => 'ok']);
         }catch(Exception $e){
             return response()->json(['error'=>$e->getMessage()]);
@@ -161,11 +162,11 @@ class IngresoController extends Controller
     }
 
     public function show($id){
-        $ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_all_Id('.$id.')')[0];
+        $ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_all_Id('.$id.')')[0];
         $ingreso = json_encode($ingreso);
-        $pacientes =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Pacientes_get_all()');
+        $pacientes =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Pacientes_get_all()');
         $pacientes = json_encode($pacientes);
-        $doctores =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Doctors_get_all()');
+        $doctores =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Doctors_get_all()');
         $doctores = json_encode($doctores);
 
         return view('ingresos.show', compact('ingreso', 'pacientes', 'doctores'));
@@ -174,11 +175,11 @@ class IngresoController extends Controller
     public function edit($id)
     {
       //Doesn't exist
-        $pacientes =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Pacientes_get_all()');
+        $pacientes =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Pacientes_get_all()');
         $pacientes = json_encode($pacientes);
-        $doctores =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Doctors_get_all()');
+        $doctores =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Doctors_get_all()');
         $doctores = json_encode($doctores);
-        $ingreso =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_get_all_Id('. $id .')')[0];
+        $ingreso =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_get_all_Id('. $id .')')[0];
         $ingreso = json_encode($ingreso);
 
         return view($this->path . '.edit', compact('pacientes', 'doctores', 'ingreso'));
@@ -207,7 +208,7 @@ class IngresoController extends Controller
     public function destroy(Request $request, $id){
     //Desn't Exist
         try{
-            $canDelete =  DB::connection(CurBD::getCurrentSchema())->statement('call OP_Ingresos_es_borrable_id('. $id .')');
+            $canDelete =  DB::connection(CurBD::getCurrentSchema())->select('exec OP_Ingresos_es_borrable_id('. $id .')');
             if( $canDelete[0]->CAN_DELETE == '1' ){
                 $ingreso = Ingreso::findOrFail($id);
                 $ingreso->is_deleted = true;
