c66f59e7c7dd45ec3660919e4c6259e9f4c3aa2c
Gian Vallejos <gian.vallejos92@gmail.com>
2020-12-14 19:53:44 -0500

    Procedures update

--- _SQL/new_procedure.sql
+++ _SQL/new_procedure.sql
@@ -34,6 +34,29 @@ BEGIN
 END
 ;;
 
+DROP PROCEDURE IF EXISTS `OP_Citas_get_all`;
+DELIMITER ;;
+CREATE PROCEDURE `OP_Citas_get_all`()
+BEGIN
+	SELECT  c.id as idEvent, 
+			CONCAT('S', idSillon, ' - ', IFNULL(pc.codigo, " "),
+            ' | Paciente: ', IFNULL(c.titulo, IFNULL(c.nota, " ")), 
+            ' | Cel: ', IFNULL(pc.celular, IFNULL(pc.celular_apoderado, IFNULL(pc.telefono, ""))),
+            ' | Doctor: ', dc.apellidos, 
+            ' | Tratamiento: ', IFNULL(tratamiento, ""), 
+            ' | Sillón ', idSillon, ' - ', sed.nombre) as title, 
+            tratamiento, idSillon, c.idPaciente, c.idDoctor, fecha,
+			CONCAT(c.fecha, ' ', c.desde) as start, 
+            CONCAT(c.fecha, ' ', c.hasta) as end, 
+            sed.nombre as nombre_sede, 
+            c.nota
+		FROM citas c
+	LEFT JOIN  sedes sed ON sed.id = c.idSede
+	LEFT JOIN pacientes as pc on pc.id = c.idPaciente
+	LEFT JOIN doctors as dc on dc.id = c.idDoctor;
+END
+;;
+
 DROP PROCEDURE IF EXISTS `OP_Citas_get_all_by_doctor_sede`;
 DELIMITER ;;
 CREATE PROCEDURE `OP_Citas_get_all_by_doctor_sede`(IN doctorId int, IN sedeId int)
